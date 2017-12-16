f30674da7ff0ee797d49936eb73620bfec896131
Moskalyov Dmitry <madmazoku@gmail.com>
2017-12-16 15:42:33 +0300

    update cmake

--- otus.lessons.01.02/src/otus.lessons.01.02.cpp
+++ otus.lessons.01.02/src/otus.lessons.01.02.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include <spdlog/spdlog.h>
+
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+	auto console = spdlog::stdout_logger_mt("console");
+	console->info("Wellcome to spdlog!");
+	
+	std::cout << "Hello World!" << std::endl;
+
+	console->info("Goodbye spdlog!");
+	getchar();
 }
